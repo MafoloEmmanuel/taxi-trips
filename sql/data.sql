ef914eadba62540c1c0a302730acc40413f6a827
MafoloEmmanuel <mafoloemmanuel1@gmail.com>
2021-12-06 14:20:07 +0200

    travis

--- sql/data.sql
+++ sql/data.sql
@@ -5,18 +5,33 @@ insert into region(id, region_name) values (2, 'Cape Town');
 insert into region(id, region_name) values (3, 'Gauteng');
 
 /* insert data into taxi table*/
-insert into taxi(reg_number,trips_no,region_id) values ('ND 23451', 7, 1);
-insert into taxi(reg_number,trips_no,region_id) values ('BGf 789 GP',3,3);
-insert into taxi(reg_number,trips_no,region_id) values ('CA 25874',9,2);
+insert into taxi(id,reg_number,trips_no,region_id) values (1,'ND 23451', 7, 1);
+insert into taxi(id,reg_number,trips_no,region_id) values (2,'ND 22074', 13, 1);
+insert into taxi(id,reg_number,trips_no,region_id) values (3,'ND 21559', 10, 1);
+
+insert into taxi(id,reg_number,trips_no,region_id) values (4,'BGf 789 GP',9,3);
+insert into taxi(id,reg_number,trips_no,region_id) values (5,'TYI 569 GP',12,3);
+insert into taxi(id,reg_number,trips_no,region_id) values (6,'FGP 320 GP',15,3);
+
+insert into taxi(id,reg_number,trips_no,region_id) values (7,'CA 25874',9,2);
+insert into taxi(id,reg_number,trips_no,region_id) values (8,'CA 22778',10,2);
+insert into taxi(id,reg_number,trips_no,region_id) values (9,'CA 20112',12,2);
+
 
 
 /* insert data into routes table*/
 
-insert into routes(name,fare) values ('Umlazi', 15.00);
-insert into routes(name,fare) values ('Johannesburg', 18.00);
-insert into routes(name,fare) values ('Ebenezer', 25.00);
-insert into routes(name,fare) values ('Springs', 20.00);
+insert into routes(id,name,fare) values (45,'Umlazi', 15.00);
+insert into routes(id,name,fare) values (55,'Johannesburg', 18.00);
+insert into routes(id,name,fare) values (65,'Ebenhaezer', 25.00);
+insert into routes(id,name,fare) values (75,'Springs', 20.00);
 
 /* insert data into trip table*/
+
+insert into trip(id,taxi_id,routes_id) values (1,1,45);
+insert into trip(id,taxi_id,routes_id) values (2,4,55);
+insert into trip(id,taxi_id,routes_id) values (3,4,65);
+insert into trip(id,taxi_id,routes_id) values (4,6,75);
+
 
 
--- sql/tables.sql
+++ sql/tables.sql
@@ -1,16 +1,13 @@
-create table trip (
-id serial not null primary key,
-taxi_id int,
-routes_id int,
-foreign key (routes_id) references routes(id),
-foreign key (taxi_id) references taxi(id)
+
+create table region (
+    id serial not null primary key,
+    region_name text
 );
 create table routes (
     id serial not null primary key,
     name text,
     fare decimal(10,2)
 );
-
 create table taxi (
 id serial not null primary key,
 reg_number varchar(10),
@@ -19,7 +16,13 @@ region_id int,
 foreign key (region_id) references region(id)
 );
 
-create table region (
-    id serial not null primary key,
-    region_name text
+create table trip (
+id serial not null primary key,
+taxi_id int,
+routes_id int,
+foreign key (routes_id) references routes(id),
+foreign key (taxi_id) references taxi(id)
 );
+
+
+
